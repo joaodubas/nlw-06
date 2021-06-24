96a3e2e9f0ce7fd83d59d383c5f4cd17315ad673
Joao P Dubas <joao.dubas@gmail.com>
2021-06-24 00:25:47 +0000

    [day-03] add mutation to create user
    1. Add input type to receive user details
    2. Add resolver to create user
    3. Configure mutation in root type
    4. Aggregate mutations in schema

--- lib/wabanex_web/resolvers/user.ex
+++ lib/wabanex_web/resolvers/user.ex
@@ -2,4 +2,6 @@ defmodule WabanexWeb.Resolvers.User do
   alias Wabanex.Users
 
   def get(%{id: user_id}, _context), do: Users.Get.call(user_id)
+
+  def create(%{input: params}, _context), do: Users.Create.call(params)
 end
