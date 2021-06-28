f07965afa43c0208d180a33ea91ea014acbce4ac
Joao P Dubas <joao.dubas@gmail.com>
2021-06-28 00:29:00 +0000

    [extra-01] use date_range to represent training period
    Adjust graphql training object and input to use date_range type as a
    representation for period field.

--- lib/wabanex_web/schema/types/training.ex
+++ lib/wabanex_web/schema/types/training.ex
@@ -1,20 +1,19 @@
 defmodule WabanexWeb.Schema.Types.Training do
   use Absinthe.Schema.Notation
 
+  import_types WabanexWeb.Schema.Types.Custom.DateRange
   import_types WabanexWeb.Schema.Types.Exercise
 
   @desc "Logic training representation"
   object :training do
     field :id, non_null(:uuid4)
-    field :start_date, non_null(:string)
-    field :end_date, :string
+    field :period, non_null(:date_range)
     field :exercises, non_null(list_of(:exercise))
   end
 
   input_object :create_training_input do
     field :user_id, non_null(:uuid4)
-    field :start_date, non_null(:string)
-    field :end_date, :string
+    field :period, non_null(:date_range)
     field :exercises, non_null(list_of(:create_exercise_input))
   end
 end
