49bafee50c6d57b4534c7eba8c8a03da97d406c9
João Paulo Dubas <joao.dubas@noreply.gitea.dubas.dev>
2022-04-21 23:29:43 +0000

    chore: upgrade all deps (#2)
    Upgrade dependencies to their latest versions.

--- mix.exs
+++ mix.exs
@@ -29,24 +29,24 @@ defmodule Wabanex.MixProject do
 
   defp deps do
     [
-      {:phoenix, "~> 1.5.9"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_dashboard, "~> 0.4"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
       {:absinthe, "~> 1.5"},
       {:absinthe_plug, "~> 1.5"},
-      {:crudry, "~> 2.4.0"},
-      {:pg_ranges, "~> 1.1"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:crudry, "~> 2.4.0"},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:ecto_sql, "~> 3.4"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
       {:junit_formatter, "~> 3.1", only: [:test]},
-      {:lcov_ex, "~> 0.2", only: [:dev, :test], runtime: false}
+      {:lcov_ex, "~> 0.2", only: [:dev, :test], runtime: false},
+      {:pg_ranges, "~> 1.1"},
+      {:phoenix, "~> 1.6.0"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:plug_cowboy, "~> 2.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
